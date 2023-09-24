3fed4ce70bf89d58b226876d3dd5ce92e2ddc4b5
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2023-09-24 14:56:20 +0300

    Add files via upload

---
+++ Loops/NumberPyramid/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+int current = 1;
+
+for (int rows = 1; rows <= n; rows++)
+{
+    for (int cols = 1; cols <= rows; cols++)
+    {
+        if (current > n)
+        {
+            break;
+        }
+        Console.Write(current + " ");
+        current++;
+    }
+    if (current > n) { break; }
+    Console.WriteLine();
+}
