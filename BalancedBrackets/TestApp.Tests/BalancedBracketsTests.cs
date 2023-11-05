397d286def2cfd2d52708bfba1e1e1257941ebeb
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2023-11-05 11:12:37 +0200

    BalancedBrackets

---
+++ BalancedBrackets/TestApp.Tests/BalancedBracketsTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        // Arrange
+        string[] input = new []{String.Empty};
+
+        // Act
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        // Arrange
+        string[] input = new []{"(", ")","(",")"};
+
+        // Act
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = new []{")","(", ")"};
+
+        // Act
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = new []{"(", ")", "(", ")", ")"};
+
+        // Act
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = new []{"(", "(","(","("};
+
+        // Act
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void Test_IsBalanced_BracketsNotInOrder_ShouldReturnFalse()
+    {
+        // Arrange
+        string[] input = new []{")", "("};
+
+        // Act
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
