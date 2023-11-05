2ffd351103f71b22d0b08b134ae3ad4668f9a56a
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2023-11-05 11:14:14 +0200

    Exam

---
+++ Exam-Regular/CalculateAverageGrade/CalculateAverageGrade.cs
@@ -0,0 +1,22 @@
+﻿int students = int.Parse(Console.ReadLine());
+//double sumOfGrades = 0;
+//for (int i = 1; i <= students; i++)
+//{
+//    double grade = double.Parse(Console.ReadLine());
+//    sumOfGrades += grade;
+//}
+
+//Console.WriteLine($"{sumOfGrades / students:F2}");
+
+Console.WriteLine($"{GetAverage(students):F2}");
+static double GetAverage(int students)
+{
+    double[] sumOfGrades = new double[students];
+    for (int i = 0; i < students; i++)
+    {
+        double grade = double.Parse(Console.ReadLine());
+        sumOfGrades[i] = grade;
+    }
+
+    return sumOfGrades.Average();
+}
---
+++ Exam-Regular/Rotations/Rotations.cs
@@ -0,0 +1,17 @@
+﻿int[] array = Console.ReadLine()
+                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToArray();
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        int temp = array[j];
+        array[j] = array[array.Length - 1];
+        array[array.Length - 1] = temp;
+    }
+}
+
+Console.WriteLine(String.Join(", ", array));
