01032e21ac050140481173d3b56ce50d7c4e3a5d
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2023-12-16 11:07:52 +0200

    new commit

--- Exam-Preparation-2.2/02-Fruits/TestApp.Tests/FruitsTests.cs
+++ Exam-Preparation-2.2/02-Fruits/TestApp.Tests/FruitsTests.cs
@@ -10,30 +10,70 @@ public class FruitsTests
     [Test]
     public void Test_GetFruitQuantity_FruitExists_ReturnsQuantity()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int>? fruitDictionary = new () { {"banana", 5 } };
+        int expected = 5;
+
+        // Act
+        int result = Fruits.GetFruitQuantity(fruitDictionary, "banana");
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFruitQuantity_FruitDoesNotExist_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int>? fruitDictionary = new () { {"banana", 5 } };
+        int expected = 0;
+
+        // Act
+        int result = Fruits.GetFruitQuantity(fruitDictionary, "lemon");
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFruitQuantity_EmptyDictionary_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int>? fruitDictionary = new () {  };
+        int expected = 0;
+
+        // Act
+        int result = Fruits.GetFruitQuantity(fruitDictionary, "banana");
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFruitQuantity_NullDictionary_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int>? fruitDictionary = null;
+        int expected = 0;
+
+        // Act
+        int result = Fruits.GetFruitQuantity(fruitDictionary, "banana");
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFruitQuantity_NullFruitName_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int>? fruitDictionary = new () { { "banana", 0 } };
+        int expected = 0;
+
+        // Act
+        int result = Fruits.GetFruitQuantity(fruitDictionary, null);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
