aead17fc222827d84b256f4a23244ceae7b5e467
Svetlana Viktomirova <86418476+Viktomirova@users.noreply.github.com>
2023-12-16 11:07:00 +0200

    new commit

--- Exam-Preparation-2.2/03-ToDo/TestApp.Tests/ToDoListTests.cs
+++ Exam-Preparation-2.2/03-ToDo/TestApp.Tests/ToDoListTests.cs
@@ -1,4 +1,8 @@
-using System;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 
 using NUnit.Framework;
 
@@ -10,40 +14,88 @@ namespace TestApp.Tests;
 public class ToDoListTests
 {
     private ToDoList _toDoList = null!;
-    
+
     [SetUp]
     public void SetUp()
     {
         this._toDoList = new();
     }
-    
+
     [Test]
-    public void Test_AddTask_TaskAddedToToDoList()
+    public void Test_AddTask_TaskAddedToToDoList() // XX
     {
-        // TODO: finish the test
+        // Arrange 
+        _toDoList.AddTask("meeting", new DateTime(2023, 12, 15));
+        string expected = $"To-Do List:{Environment.NewLine}[ ] meeting - Due: 12/15/2023";
+
+        // Act
+        string result = _toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void Test_CompleteTask_TaskMarkedAsCompleted()
+    public void Test_CompleteTask_TaskMarkedAsCompleted() // XX
     {
-        // TODO: finish the test
+        // Arrange 
+        TaskItem task = new("meeting", new DateTime(2023, 12, 15));
+        _toDoList.AddTask(task.Title, task.DueDate);
+        string expected = $"To-Do List:{Environment.NewLine}[✓] meeting - Due: 12/15/2023";
+
+        // Act
+        _toDoList.CompleteTask(task.Title);
+
+        // Assert
+        Assert.That(_toDoList.DisplayTasks, Is.EqualTo(expected));
+        //Assert.That(task.IsCompleted, Is.False);
     }
 
     [Test]
     public void Test_CompleteTask_TaskNotFound_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        // Arrange 
+        _toDoList.AddTask("meeting", new DateTime(2023, 12, 15));
+
+        // Act
+
+        // Assert
+        //Assert.That(() => _toDoList.CompleteTask("conference"), Throws.InstanceOf<ArgumentException>());
+        Assert.Throws<ArgumentException>(() => _toDoList.CompleteTask("conference"), "Task with given title does not exist.");
     }
 
     [Test]
     public void Test_DisplayTasks_NoTasks_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        // Arrange 
+        string expected = $"To-Do List:";
+
+        // Act
+        string result = _toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void Test_DisplayTasks_WithTasks_ReturnsFormattedToDoList()
+    public void Test_DisplayTasks_WithTasks_ReturnsFormattedToDoList() // XX
     {
-        // TODO: finish the test
+        // Arrange 
+        _toDoList.AddTask("meeting", new DateTime(2023, 12, 15));
+        _toDoList.AddTask("conference", new DateTime(2023, 12, 20));
+        _toDoList.CompleteTask("meeting");
+
+        StringBuilder sb = new();        
+        sb.AppendLine("To-Do List:");
+        sb.AppendLine($"[✓] meeting - Due: 12/15/2023");
+        sb.AppendLine($"[ ] conference - Due: 12/20/2023");
+        
+        string expected = sb.ToString().Trim();
+
+        // Act
+        string result = _toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
